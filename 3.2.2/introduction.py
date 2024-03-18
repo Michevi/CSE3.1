9fa68a1625cf472ed6a7af30cf29e40d8c5659cb
Michael H <michael.hevi@vtsd.com>
2024-03-18 13:54:27 -0400

    3/18/24

---
+++ 3.2.2/introduction.py
@@ -0,0 +1,25 @@
+import random
+
+def random_number():
+
+x = 0
+while x !=100:
+    for x in range(12):
+n1 = random.randint(30,50)
+print(n1)
+
+n1 = random
+
+for x in range(12):
+    random_number()
+def largerz_val(num1, num2):
+    if num1 > num2:
+        print("larger value is: " + num1)
+    elif num1 == num2:
+        print("they are the same")
+    else:
+        print("Larger value is: " + num2)
+
+larger_val(14, 22)
+
+print(biggest_number)
