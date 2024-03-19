9ce9f02b3e4972b2dd56f2e909a890c0a92ed537
Michael H <michael.hevi@vtsd.com>
2024-03-19 13:11:43 -0400

    3/18/24

--- 3.2.2/introduction.py
+++ 3.2.2/introduction.py
@@ -1,25 +1,34 @@
 import random
-
+'''
+########################## Basic user-generated function Start
 def random_number():
-
-x = 0
-while x !=100:
-    for x in range(12):
-n1 = random.randint(30,50)
-print(n1)
-
-n1 = random
+    n1 = random.randint(30, 50)
+    print(n1)
 
 for x in range(12):
     random_number()
-def largerz_val(num1, num2):
-    if num1 > num2:
-        print("larger value is: " + num1)
-    elif num1 == num2:
-        print("they are the same")
-    else:
-        print("Larger value is: " + num2)
 
-larger_val(14, 22)
+x = 0
+while x != 100:
+    random_number()
+    x += 1
+
+###################
+####### Basic user-generated function End
+
+########################## Basic Parameters Start
+'''
+def larger_val(num1, num2):
+    if num1 > num2:
+
+        return num1
+    elif num1 == num2:
+        print("They are the same")
+        return num2
+    else:
+        return num2
+
+
+biggest_number = larger_val(14, 22)
 
 print(biggest_number)
---
+++ 3.2.2/main.py
@@ -0,0 +1,18 @@
+from Post import Post
+
+all_posts_archive = []
+
+# your code here
+
+post1 = Post("Marie", " This is my first post!")
+
+print(post1)
+
+post2 = Post("Marie", " This is my second")
+
+post3 = Post("Marie", " This is my third")
+
+username = input("Enter the username")
+
+user_input = input("Enter the options below new - add a post to the archive")
+
