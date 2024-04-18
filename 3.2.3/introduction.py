7153e72ddea114e3037e651d0082fb58118ed6fa
Michael H <michael.hevi@vtsd.com>
2024-04-18 13:26:41 -0400

    3/18/24

---
+++ 3.2.3/introduction.py
@@ -0,0 +1,15 @@
+ # basic "FOR" loop
+
+ #looping threw a specific list
+num_list = [42, 6, 11, 14, 7, 89, 17]
+
+for numbers in num_list:
+    print(numbers - 13)
+
+#looping a specific number of times
+for battered in range(14):
+    print("sausage")
+
+    #how to use step in rnage functions
+    test_list = range(2, 14, 2)
+    print(test_list)
